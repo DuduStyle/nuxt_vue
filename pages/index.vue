9b4f4475380ca8e712cf555163f2c88a1f17a8c7
xinbo_zhang <xinbo_zhang@163.com>
2019-10-25 20:35:12 +0800

    feat: 加上数据内容

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,56 @@
 <template>
-  <!-- <div class="container"> -->
-  <!-- banner -->
-  <el-carousel class="banner" trigger="click" :height="clientHeight">
-    <el-carousel-item v-for="item in images" :key="item">
-      <div
-        class="item_bg image"
-        style="backgroundImage: url('http://resources.jsmo.xin/templates/upload/16886/201910/1571023199395.jpg')"
-      >
-        <!-- <img :src="item"> -->
-      </div>
-    </el-carousel-item>
-  </el-carousel>
-  <!-- </div> -->
+  <div>
+    <!-- banner -->
+    <el-carousel class="banner" trigger="click" :height="clientHeight">
+      <el-carousel-item v-for="item in images" :key="item">
+        <div
+          class="item_bg image"
+          style="backgroundImage: url('http://resources.jsmo.xin/templates/upload/16886/201910/1571023199395.jpg')"
+        >
+          <!-- <img :src="item"> -->
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+    <!-- 数字介绍 -->
+    <el-row class="container-gutter ">
+      <el-col :span="6">
+        <div class="grid-content bg-purple content_list">
+          <div class="number">
+             <span>8</span>
+            <span>年</span>
+          </div>
+          <div class="title">专注设计</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+         <div class="grid-content bg-purple content_list">
+          <div class="number">
+             <span>12</span>
+            <span>+</span>
+          </div>
+          <div class="title">设计师</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+         <div class="grid-content bg-purple content_list">
+          <div class="number">
+             <span>820</span>
+            <span>+</span>
+          </div>
+          <div class="title">服务客户</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+         <div class="grid-content bg-purple content_list">
+          <div class="number">
+            <span>2,100</span>
+            <span>+</span>
+          </div>
+          <div class="title">作品案例</div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -51,20 +90,31 @@ export default {
     background-repeat: no-repeat;
     // background-image: url(http://resources.jsmo.xin/templates/upload/16886/201910/1571023199395.jpg);
   }
-}
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-}
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
 
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+}
+.container-gutter {
+  padding: 100px 0;
+  .content_list {
+    .number {
+      text-align: center;
+    }
+    .title {
+      text-align: center;
+    }
+  }
 }
 </style>
